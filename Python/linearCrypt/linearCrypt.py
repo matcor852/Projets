e595839dad08d4de93708e47184c38e175830a44
matcor852 <62258645+matcor852@users.noreply.github.com>
2021-03-13 16:12:13 +0100

    Update linearCrypt.py

--- Python/linearCrypt/linearCrypt.py
+++ Python/linearCrypt/linearCrypt.py
@@ -1,9 +1,5 @@
 import string, sys
 
-def nbDigits(x):
-    #return number of digits in x
-    return len("%i"%x)
-
 def pgcd(a,b):
     while b!=0:
         a,b=b,a%b
@@ -22,9 +18,8 @@ m = 3
 p = 6
 plain = "linear crypt"
 
-ref = string.ascii_letters + string.whitespace + string.digits + string.punctuation
+ref = string.ascii_letters + "éèçàù " + string.digits + string.punctuation
 mod = len(ref)
-max_l = nbDigits(mod)
 
 if pgcd(m,mod) != 1:
     print("base is not invertible for the given modulus; pgcd of 'm' and 'mod' must equal 1.")
@@ -34,14 +29,12 @@ if pgcd(m,mod) != 1:
 #crypt
 cipher = ""
 for char in plain:
-    cipher_char = linearCrypt(m,ref.index(char),p,mod)
-    cipher += '0'*(max_l-nbDigits(cipher_char)) + str(cipher_char)
+    cipher += ref[linearCrypt(m,ref.index(char),p,mod)]
 print("cipher : ", cipher)
 
 
 #decrypt
 plaintext = ""
-for i in range(int(len(cipher)/max_l)):
-    pos = i*max_l
-    plaintext += ref[int(linearDecrypt(m,int(cipher[pos:pos+max_l]),p,mod))]
+for char in cipher:
+    plaintext += ref[int(linearDecrypt(m,ref.index(char),p,mod))]
 print("plaintext : ",plaintext)
